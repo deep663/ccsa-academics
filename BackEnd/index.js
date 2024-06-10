d6f08295182d03a58d9bded8b40ebb84cf1b3ba0
Deep <deepsenchowa1@gmail.com>
2024-06-10 09:21:02 +0530

    lab delete

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -24,6 +24,9 @@ app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
 
+
+
+
 //multer
 
 const multer = require("multer");
--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Assignment from "./pages/Assignment";
 import Dashboard from "./pages/Dashboard";
 import FinalResults from "./pages/FinalResults";
 import InsemMarks from "./pages/InsemMarks";
-import Lab from "./pages/Lab";
 import About from "./pages/About";
 import Pagenotfound from "./pages/Pagenotfound";
 import TeacherReg from "./components/TeacherReg";
@@ -33,7 +32,6 @@ function App() {
           <Route path="/assignment" element={<Assignment />} />
           <Route path="/finalresults" element={<FinalResults />} />
           <Route path="/insemmarks" element={<InsemMarks />} />
-          <Route path="/lab" element={<Lab />} />
         </Route>
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
